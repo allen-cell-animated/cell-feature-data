8fd4f11a0d14879f2782c33972185ab68294e461
meganrm <meganrm@gmail.com>
2021-02-24 14:44:17 -0800

    extract out varible for dir path

--- process-dataset/steps/write-per-cell-jsons.js
+++ process-dataset/steps/write-per-cell-jsons.js
@@ -4,7 +4,7 @@ const {
 } = require("lodash");
 
 const schemas = require("../data-validation/schema");
-const dataPrep = require ("../data-validation/data-prep");
+const dataPrep = require("../data-validation/data-prep");
 const {
     CELL_LINE_NAME_KEY,
     FILE_INFO_KEYS,
@@ -27,34 +27,35 @@ const formatAndWritePerCellJsons = async (readFolder, outFolder, featureDataFile
             const fileInfoJson = [];
             for (let index = 0; index < json.length; index++) {
                 const cellData = json[index];
-                
+
                 if (cellData.file_info.length !== FILE_INFO_KEYS.length) {
                     console.error("file info in not in expected format")
                     process.exit(1)
                 }
                 const fileInfo = cellData.file_info.reduce((acc, cur, index) => {
-                    
+
                     acc[FILE_INFO_KEYS[index]] = cur;
                     return acc;
                 }, {});
                 const cellLine = find(cellLines, {
-                            [CELL_LINE_DEF_NAME_KEY]: fileInfo[CELL_LINE_NAME_KEY]
-                            });
+                    [CELL_LINE_DEF_NAME_KEY]: fileInfo[CELL_LINE_NAME_KEY]
+                });
                 if (!cellLine) {
                     console.error("No matching cell line for this cell in the dataset", fileInfo)
                     process.exit(1)
                 }
+                const dirPath = `/${cellLine.CellLineId_Name}/${cellLine.CellLineId_Name}_${fileInfo[FOV_ID_KEY]}`
                 if (!fileInfo.thumbnailPath) {
-                        fileInfo.thumbnailPath = `/${cellLine.CellLineId_Name}/${cellLine.CellLineId_Name}_${fileInfo[FOV_ID_KEY]}_${fileInfo[CELL_ID_KEY]}.png`;
+                    fileInfo.thumbnailPath = `${dirPath}_${fileInfo[CELL_ID_KEY]}.png`;
                 }
                 if (!fileInfo.fovThumbnailPath) {
-                    fileInfo.fovThumbnailPath = `/${cellLine.CellLineId_Name}/${cellLine.CellLineId_Name}_${fileInfo[FOV_ID_KEY]}.png`;
+                    fileInfo.fovThumbnailPath = `${dirPath}.png`;
                 }
                 if (!fileInfo.volumeviewerPath) {
-                    fileInfo.volumeviewerPath = `/${cellLine.CellLineId_Name}/${cellLine.CellLineId_Name}_${fileInfo[FOV_ID_KEY]}_${fileInfo[CELL_ID_KEY]}.json`;
+                    fileInfo.volumeviewerPath = `${dirPath}_${fileInfo[CELL_ID_KEY]}.json`;
                 }
                 if (!fileInfo.fovVolumeviewerPath) {
-                    fileInfo.fovVolumeviewerPath = `/${cellLine.CellLineId_Name}/${cellLine.CellLineId_Name}_${fileInfo[FOV_ID_KEY]}.json`;
+                    fileInfo.fovVolumeviewerPath = `${dirPath}.json`;
                 }
                 fileInfoJson[index] = fileInfo;
 
@@ -66,14 +67,15 @@ const formatAndWritePerCellJsons = async (readFolder, outFolder, featureDataFile
                 }
 
             }
-            
+
             const fileInfoCheck = dataPrep.validate(fileInfoJson, schemas.fileInfo);
 
             const measuredFeaturesCheck = dataPrep.validate(measuredFeaturesJson, schemas.measuredFeaturesDoc)
             if (fileInfoCheck.valid && measuredFeaturesCheck) {
-                
+
                 return Promise.all([fsPromises.writeFile(`${outFolder}/${TEMP_LOCAL_CELL_FEATURE_JSON}`, JSON.stringify(measuredFeaturesJson)),
-                 fsPromises.writeFile(`${outFolder}/${TEMP_LOCAL_FILE_INFO_JSON}`, JSON.stringify(fileInfoJson))])
+                    fsPromises.writeFile(`${outFolder}/${TEMP_LOCAL_FILE_INFO_JSON}`, JSON.stringify(fileInfoJson))
+                ])
             } else {
                 console.error("failed json validation")
                 if (fileInfoCheck.error) {
