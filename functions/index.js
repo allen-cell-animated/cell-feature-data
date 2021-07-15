3e02edf017c74ce941a7027280a15958ca833276
meganrm <meganrm@gmail.com>
2021-07-15 12:29:35 -0700

    post to slack on manifest update

--- functions/index.js
+++ functions/index.js
@@ -1,78 +1,25 @@
-/**
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-'use strict';
-
-// [START functionsimport]
 const functions = require('firebase-functions');
-// [END functionsimport]
-// [START additionalimports]
-// Moments library to format dates.
-const moment = require('moment');
-// CORS Express middleware to enable CORS Requests.
-const cors = require('cors')({
-    origin: true,
-});
-// [END additionalimports]
 
-// [START all]
-/**
- * Returns the server's date. You must provide a `format` URL query parameter or `format` value in
- * the request body with which we'll try to format the date.
- *
- * Format must follow the Node moment library. See: http://momentjs.com/
- *
- * Example format: "MMMM Do YYYY, h:mm:ss a".
- * Example request using URL query parameters:
- *   https://us-central1-<project-id>.cloudfunctions.net/date?format=MMMM%20Do%20YYYY%2C%20h%3Amm%3Ass%20a
- * Example request using request body with cURL:
- *   curl -H 'Content-Type: application/json' /
- *        -d '{"format": "MMMM Do YYYY, h:mm:ss a"}' /
- *        https://us-central1-<project-id>.cloudfunctions.net/date
- *
- * This endpoint supports CORS.
- */
-// [START trigger]
-exports.date = functions.https.onRequest((req, res) => {
-    // [END trigger]
-    // [START sendError]
-    // Forbidding PUT requests.
-    if (req.method === 'PUT') {
-        return res.status(403).send('Forbidden!');
-    }
-    // [END sendError]
+const axios = require('axios')
 
-    // [START usingMiddleware]
-    // Enable CORS using the `cors` express middleware.
-    return cors(req, res, () => {
-        // [END usingMiddleware]
-        // Reading date format from URL query parameter.
-        // [START readQueryParam]
-        let format = req.query.format;
-        // [END readQueryParam]
-        // Reading date format from request body query parameter
-        if (!format) {
-            // [START readBodyParam]
-            format = req.body.format;
-            // [END readBodyParam]
+const webhook = functions.config().slack.webhook
+
+exports.postChangesToManifest = functions.firestore
+    .document('manifests/{docId}')
+    .onUpdate((change, context) => {
+        const newValue = change.after.data();
+        const previousValue = change.before.data();
+        const body = {
+            username: "firebase-bot",
+            text: `Updated: ${JSON.stringify(context.params.docId)}, Previous value ${JSON.stringify(previousValue)}, New value: ${JSON.stringify(newValue)}`,
         }
-        // [START sendResponse]
-        const formattedDate = moment().format(`${format}`);
-        functions.logger.log('Sending Formatted date:', formattedDate);
-        res.status(200).send(formattedDate);
-        // [END sendResponse]
+        return axios.post(webhook, body).then(res => {
+                console.log(`statusCode: ${res.status}`)
+                console.log(res)
+            })
+            .catch(error => {
+                console.error("ERROR", error.response.data)
+                console.error("ERROR", error.response)
+
+            })
     });
-});
-// [END all]
