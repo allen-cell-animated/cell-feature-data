b9a7a19c74aa59ab321d4f2f243e50f54f123a85
meganrm <meganrm@gmail.com>
2022-02-15 15:56:46 -0800

    remove cell lines and path info

--- process-dataset/data-validation/schema.js
+++ process-dataset/data-validation/schema.js
@@ -64,10 +64,6 @@ const manifestSchema = {
             "description": "url to the per cell data json",
             "type": "string"
         },
-        "cellLineDataPath": {
-            "description": "collection name of the cell line data",
-            "type": "string",
-        },
         "featureDefsPath": {
             "description": "path to the feature def collection",
             "type": "string",
@@ -171,7 +167,6 @@ const manifestSchema = {
     },
     "required": [
         "featuresDataPath",
-        "cellLineDataPath",
         "viewerSettingsPath",
         "albumPath",
         "thumbnailRoot",
