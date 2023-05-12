b40719eec68ad5f3dde146f352fe49dce256c665
Ruge Li <rugeli0605@gmail.com>
2023-05-12 15:31:22 -0700

    add validation in check input dataset

--- bin/check-input-datasets.js
+++ bin/check-input-datasets.js
@@ -3,10 +3,9 @@ const {
   getInputDatasetSchema,
 } = require("../src/data-validation/get-input-dataset-schema");
 const { DATA_FOLDER_NAME } = require("../src/process-single-dataset/constants");
-const { readDatasetJson } = require("../src/utils");
+const utils = require("../src/utils");
 const dataPrep = require("../src/data-validation/data-prep");
 const unpackInputDataset = require("../src/data-validation/unpack-input-dataset");
-const {validateFeatureDataKeys} = require("../src/utils");
 // referenced partial schemas
 const INPUT_DATASET_SCHEMA_FILE = "input-dataset.schema.json";
 const INPUT_MEGASET_SCHEMA_FILE = "input-megaset.schema.json";
@@ -17,22 +16,34 @@ const checkForError = (fileName, json, schemaFileName) => {
     json,
     ajv.getSchema(schemaFileName)
   );
-  let featureKeysError = false;
-  let keysErrorMsg = "";
+  let datasetsError = false;
+  let ErrorMsg = "";
   if (json["feature-defs"]) {
     const featuresDataOrder = json.dataset.featuresDataOrder;
     const featureDefsData = json["feature-defs"];
-    const result = validateFeatureDataKeys( featuresDataOrder, featureDefsData );
-    featureKeysError = result.featureKeysError;
-    keysErrorMsg = result.keysErrorMsg;
+    const result = utils.validateFeatureDataKeys( featuresDataOrder, featureDefsData );
+    if (result.featureKeysError) {
+      datasetsError = result.featureKeysError;
+      ErrorMsg = result.keysErrorMsg;
+    };
+  };
+  if (json["dataset"]) {
+    const totalCells = json.dataset.userData.totalCells;
+    const totalFOVs = json.dataset.userData.totalFOVs;
+    const result = utils.validateUserDataValues( totalCells, totalFOVs );
+    if (result.userDataError) {
+      datasetsError = result.userDataError;
+      ErrorMsg = result.userDataErrorMsg;
+    };  
   };
 
-  if (!valid || featureKeysError) {
+
+  if (!valid || datasetsError) {
     console.log(
       "\x1b[0m",
       `${fileName}`,
       "\x1b[31m",
-      `failed because: ${JSON.stringify(error || keysErrorMsg)}`,
+      `failed because: ${JSON.stringify(error || ErrorMsg)}`,
       "\x1b[0m"
     );
     return true;
@@ -79,7 +90,7 @@ const validateDatasets = () => {
         }
       }
       for (const datasetFolder of foldersToCheck) {
-        const topLevelJson = await readDatasetJson(datasetFolder);
+        const topLevelJson = await utils.readDatasetJson(datasetFolder);
         if (topLevelJson.datasets) {
           // is a megaset, need to check both megaset
           // file and each dataset folder
