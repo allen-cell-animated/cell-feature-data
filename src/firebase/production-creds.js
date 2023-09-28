91a47fc92744613124ac49fa59a78b5abdf964ce
Ruge Li <rugeli0605@gmail.com>
2023-09-28 16:49:10 -0700

    exit if no token or email in dotenv for prod

--- src/firebase/production-creds.js
+++ src/firebase/production-creds.js
@@ -3,6 +3,19 @@ const FIREBASE_ID= "allen-cell-resource";
 const FIREBASE_DB_URL = `https://${FIREBASE_ID}.firebaseapp.com`;
 const FIREBASE_EMAIL= process.env.FIREBASE_EMAIL
 
+  if (!FIREBASE_TOKEN) {
+    console.error(
+      "You need a secret token FIREBASE_TOKEN to use the production database"
+    );
+    process.exit(1);
+  }
+  if (!FIREBASE_EMAIL) {
+    console.error(
+      "You need a secret email FIREBASE_EMAIL to use the production database"
+    );
+    process.exit(1);
+  }
+
 module.exports = {
   FIREBASE_TOKEN,
   FIREBASE_ID,
