3d5edf42b9d2ef84c86d6f2c60bbdf3ef75ba113
Ruge Li <rugeli0605@gmail.com>
2023-09-28 17:34:37 -0700

    test

--- src/firebase/setup-firebase.js
+++ src/firebase/setup-firebase.js
@@ -12,19 +12,15 @@ if (notProduction) {
 
   ref = require("./production-creds");
 }
-const { FIREBASE_TOKEN, FIREBASE_ID, FIREBASE_DB_URL,FIREBASE_EMAIL } = ref;
-const check_token = FIREBASE_TOKEN
-console.log("check_token", check_token);
-const firebasekey = FIREBASE_TOKEN.replace(/\\n/g, "\n");
-const firebasekey_without_quotes = firebasekey.replace(/^"|"$/g, "");
-console.log("firebasekey", firebasekey);
+const { FIREBASE_TOKEN, FIREBASE_ID, FIREBASE_DB_URL, FIREBASE_EMAIL } = ref;
+const firebasekey = FIREBASE_TOKEN.replace(/\\n/g, "\n").trim().replace(/^"|"$/g, "");
 
 var app = admin.initializeApp({
   credential: admin.credential.cert({
     type: "service_account",
     projectId: FIREBASE_ID,
     clientEmail: FIREBASE_EMAIL,
-    privateKey: firebasekey_without_quotes,
+    privateKey: firebasekey,
   }),
   databaseURL: FIREBASE_DB_URL,
 });
