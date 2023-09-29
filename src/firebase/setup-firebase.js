e7b34f78ea737446cce6768821e4b27efb903839
Ruge Li <rugeli0605@gmail.com>
2023-09-28 18:30:21 -0700

    test

--- src/firebase/setup-firebase.js
+++ src/firebase/setup-firebase.js
@@ -14,7 +14,7 @@ if (notProduction) {
 }
 const { FIREBASE_TOKEN, FIREBASE_ID, FIREBASE_DB_URL, FIREBASE_EMAIL } = ref;
 const firebasekey = FIREBASE_TOKEN.replace(/\\n/g, "\n").trim().replace(/^"|"$/g, "");
-console.log("FIREBASE_TOKEN", FIREBASE_TOKEN);
+console.log("firebasekey", firebasekey);
 
 var app = admin.initializeApp({
   credential: admin.credential.cert({
