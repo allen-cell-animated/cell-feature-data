dad7845aab843ef578725b44a07fba0c2c7f6b78
Ruge Li <91452427+rugeli@users.noreply.github.com>
2023-11-01 14:51:16 -0700

    python dataset processor: initial settings and read csv (#95)
    * initial setup
    
    * draft - load data and create dataset folder
    
    * add docstrings
    
    * rename test data
    
    * rename
    
    * refactor: separate code into loader and writer
    
    * delete print statements
    
    * reformat

---
+++ dataset-processor-python/data_loader.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import csv
+
+
+class DataLoader:
+    def __init__(self, path, dataset_name):
+        self.path = path
+        self.dataset_name = dataset_name
+        self.initial_data = None
+
+    def read_csv(self):
+        """
+        read the csv file and store the data
+        """
+        try:
+            with open(self.path, "r") as f:
+                reader = csv.DictReader(f)
+                data = list(
+                    reader
+                )  # data is a list of dictionaries, each dictionary is a row
+                self.initial_data = data
+        except Exception as e:
+            print(f"Error while reading CSV: {e}")
+
+
+class DatasetWriter(DataLoader):
+    """
+    Class to create the dataset folder and write json files
+    """
+
+    def __init__(self, path, dataset_name):
+        super().__init__(path, dataset_name)
+
+    def get_inputs(self):
+        """
+        get the inputs from the user
+        """
+        # version is required, title and description are optional, default values are set as empty string
+        # return version, title, description
+        pass
+
+    def create_dataset_folder(self):
+        """
+        create a folder with the database_name
+        initialize the folder with required json files
+        """
+        # folder_name = self.dataset_name + "_" + version
+        # path = os.path.join("data", folder_name)
+        # os.makedirs(path, exist_ok=True)
+        # create json files -> feature_defs.json, dataset.json, cell_feature_analysis.json, image_settings.json
+        pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+    else:
+        file_path = input("Enter the file path: ")
+    dataset_name = os.path.splitext(os.path.basename(file_path))[0]
+    manager = DatasetWriter(file_path, dataset_name)
+    manager.read_csv()
