518467d33acc244a01adc8e046bf473207bfe62c
Ruge Li <rugeli0605@gmail.com>
2023-11-14 16:54:04 -0800

    rename and refactor

--- dataset-processor-python/data_loader.py
+++ dataset-processor-python/data_loader.py
@@ -20,13 +20,12 @@ class DataLoader:
     def __init__(self, path, dataset_name):
         self.path = path
         self.dataset_name = dataset_name
-        self.initial_data = self.read_csv()
+        self.data = self.load_csv()
 
-    def read_csv(self):
+    def load_csv(self):
         """
         read the csv file and store the data
         """
-        print("Reading the CSV file...")
         try:
             with open(self.path, "r") as f:
                 reader = csv.DictReader(f)
@@ -50,82 +49,70 @@ class DatasetWriter(DataLoader):
         self.discrete_features_dict = {}
         self.features_data_order = []
 
-    def get_inputs(self):
-        """
-        get the inputs from the user
-        """
-        # TODO: clarify the version format, input validation needed
-        self.version = input("Enter the version in the format: ").strip()
-        if not self.version:
-            print("Please enter a valid version.")
+    def get_user_inputs(self):
+        self.version = input("Enter the version: ").strip()
+        if not self.version or not self.is_valid_version(self.version):
+            print("Please enter a valid version in the format [yyyy.number].")
             # recursively call to prompt the user
-            return self.get_inputs()
-        else:
-            self.title = input("Enter the title: ").strip()
-            self.description = input("Enter the description: ").strip()
+            return self.get_user_inputs()
+        self.title = input("Enter the title: ").strip()
+        self.description = input("Enter the description: ").strip()
 
-    def sort_data(self):
-        for row in self.initial_data:
-            file_info = []
-            features = []
-            for column, value in row.items():
-                if column in self.FILEINFO_COLUMN_NAMES:
-                    value = self._convert_str_to_num(value)
-                    file_info.append(value)
-                else:
-                    value = self._convert_str_to_num(value)
-                    if (
-                        pd.isna(value)
-                        or isinstance(value, int)
-                        or isinstance(value, float)
-                    ):
-                        features.append(value)
-                        is_discrete = self._is_discrete(value)
-                        self.discrete_features_dict[column] = is_discrete
-                        if column not in self.feature_def_keys:
-                            self.feature_def_keys.append(column)
-                    else:
-                        print(f"Invalid value: {value} in column {column}")
-                        sys.exit(1)
-            self.prepare_cell_feature_analysis(file_info, features)
+    def is_valid_version(self, version):
+        pattern = r"^\d{4}\.[0-9]+$"
+        return re.match(pattern, version) is not None
+
+    def process_data(self):
+        for row in self.data:
+            file_info, features = self.get_row_data(row)
+            self.update_cell_feature_analysis(file_info, features)
         self.prepare_feature_defs()
         self.prepare_dataset()
 
-    def _is_discrete(self, numeric_value):
-        if pd.isna(numeric_value):
-            return False
-        return numeric_value == int(numeric_value)
+    def get_row_data(self, row):
+        file_info = []
+        features = []
+        for column, value in row.items():
+            value = self.convert_str_to_num(value)
+            if column in self.FILEINFO_COLUMN_NAMES:
+                file_info.append(value)
+            else:
+                if self.is_valid_feature_value(value):
+                    features.append(value)
+                    self.update_feature_metadata(column, value)
+                else:
+                    print(f"Invalid value: {value} in column {column}")
+                    sys.exit(1)
+        return file_info, features
 
-    def _convert_str_to_num(self, value):
+    def convert_str_to_num(self, value):
         try:
             numeric_value = float(value)
             if numeric_value.is_integer():
                 return int(numeric_value)
             return numeric_value
         except ValueError:
-            # return the original value
+            # if not number, return the original value
             return value
 
-    def prepare_cell_feature_analysis(self, file_info, features):
+    def is_valid_feature_value(self, value):
+        return pd.isna(value) or isinstance(value, (int, float))
+
+    def update_feature_metadata(self, column, value):
+        is_discrete = self.is_discrete(value)
+        self.discrete_features_dict[column] = is_discrete
+        if column not in self.feature_def_keys:
+            self.feature_def_keys.append(column)
+
+    def is_discrete(self, numeric_value):
+        if pd.isna(numeric_value):
+            return False
+        return numeric_value == int(numeric_value)
+
+    def update_cell_feature_analysis(self, file_info, features):
         self.cell_feature_analysis_file.append(
             {"file_info": file_info, "features": features}
         )
-
-    @staticmethod
-    def _title_except_small_words(title):
-        lowercase_list = ["the", "for", "in", "of", "on", "to", "and", "as", "or"]
-        return " ".join(
-            [word if word in lowercase_list else word.title() for word in title.split()]
-        )
-
-    def _has_unit(self, key):
-        has_unit = re.search(r"\((.*?)\)", key)
-        if has_unit:
-            # get the unit from the key
-            unit = has_unit.group(1)
-        else:
-            unit = ""
-        return unit
 
     def prepare_feature_defs(self):
         description = ""
@@ -133,51 +120,74 @@ class DatasetWriter(DataLoader):
         discrete = None
         for key in self.feature_def_keys:
             discrete = self.discrete_features_dict[key]
-            unit = self._has_unit(key)
+            unit = self.get_unit(key)
             key = key.replace(f"({unit})", "").strip()
             self.features_data_order.append(key)
-            display_name = DatasetWriter._title_except_small_words(
-                key.replace("-", " ")
-            )
-            self.feature_defs_file.append(
-                {
-                    "key": key,
-                    "displayName": display_name,
-                    "unit": unit,
-                    "description": description,
-                    "tooltip": tooltip,
-                    "discrete": discrete,
-                }
-            )
+            display_name = DatasetWriter.format_display_name(key.replace("-", " "))
+            feature_def = {
+                "key": key,
+                "displayName": display_name,
+                "unit": unit,
+                "description": description,
+                "tooltip": tooltip,
+                "discrete": discrete
+            }
+            if discrete:
+                options = self.prepare_discrete_feature_options(key)
+                feature_def["options"] = options
+            self.feature_defs_file.append(feature_def)
+
+    @staticmethod
+    def format_display_name(title):
+        lowercase_list = ["the", "for", "in", "of", "on", "to", "and", "as", "or"]
+        return " ".join(
+            word if word in lowercase_list else word.title() for word in title.split()
+        )
+
+    @staticmethod
+    def get_unit(key):
+        match = re.search(r"\((.*?)\)", key)
+        if match:
+            # get the unit from the key
+            unit = match.group(1)
+        else:
+            unit = ""
+        return unit
+
+    def prepare_discrete_feature_options(self, key):
+        # TODO: write the options dict for discrete features
+        # test option data
+        return {
+            "0": {"color": "#fed98e", "name": "Incomplete"},
+            "1": {"color": "#7f48f3", "name": "Complete"},
+            "-1": {"color": "#838383", "name": "Not determined"},
+        }
 
     def prepare_dataset(self):
         # TODO: get more input data from users
         fields_to_write = {
-            "version": self.version,
             "title": self.title,
+            "version": self.version,
+            "name": self.dataset_name,
             "description": self.description,
             "featuresDataOrder": self.features_data_order,
         }
         self.dataset_file.update(fields_to_write)
         return self.dataset_file
 
-    def create_dataset(self):
-        """
-        create a folder with the database_name
-        initialize the folder with required json files
-        """
-        print("Creating the dataset folder...")
-        # TODO: clarify the folder name and dataset name
-        folder_name = f"dataset-{self.dataset_name}-v{self.version}"
+    def create_dataset_folder(self):
+        folder_name = f"{self.dataset_name}_v{self.version}"
         path = os.path.join("data", folder_name)
         os.makedirs(path, exist_ok=True)
+        self.write_json_files(path)
+
+    def write_json_files(self, path):
         json_files = {
             "cell_feature_analysis.json": self.cell_feature_analysis_file,
             "feature_defs.json": self.feature_defs_file,
             "dataset.json": self.dataset_file,
             "image_settings.json": {},
         }
-
         for file_name, data in json_files.items():
             file_path = os.path.join(path, file_name)
             with open(file_path, "w") as f:
@@ -191,6 +201,6 @@ if __name__ == "__main__":
         file_path = input("Enter the file path: ")
     dataset_name = os.path.splitext(os.path.basename(file_path))[0]
     writer = DatasetWriter(file_path, dataset_name)
-    writer.get_inputs()
-    writer.sort_data()
-    writer.create_dataset()
+    writer.get_user_inputs()
+    writer.process_data()
+    writer.create_dataset_folder()
