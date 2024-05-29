7c96880d227ca817353b1e87e4e4a701ea92b986
Ruge Li <rugeli0605@gmail.com>
2024-05-28 21:36:22 -0700

    add megaset file name to constants

--- cell_feature_data/constants.py
+++ cell_feature_data/constants.py
@@ -2,6 +2,7 @@ CELL_FEATURE_ANALYSIS_FILENAME = "cell_feature_analysis.json"
 DATASET_FILENAME = "dataset.json"
 FEATURE_DEFS_FILENAME = "feature_defs.json"
 IMAGE_SETTINGS_FILENAME = "image_settings.json"
+MEGASET_DATASET_FILENAME = "dataset.json"
 
 FILEINFO_COLUMN_NAMES = [
     "cell_id",
